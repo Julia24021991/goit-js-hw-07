a8c19f33442e70cf9cf452773a985ded666e855e
Julia24021991 <yuliya.kovalyk@gmail.com>
2023-11-08 11:43:44 +0200

    commit2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const galleryListItem = galleryListImg(galleryItems);
+
+function galleryListImg(galleryItem) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+         <a class="gallery__link" href="${original}">
+         <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+    />
+  </a>
+</li>`;
+    }).join("");
+}
+
+galleryList.insertAdjacentHTML("beforeend", galleryListItem);
+
+const galleryListClick = (event) => {
+    event.preventDefault();
+    const source = event.target.dataset.source;
+    if (!source) return;
+    const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`, {
+        onShow: () => {
+            window.addEventListener('keydown', onKeydownEsc);
+        },
+        onClose: () => {
+            window.removeEventListener('keydown', onKeydownEsc);
+        }
+    });
+    const onKeydownEsc = (event) => {
+        console.log(event.code);
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    };
+
+    instance.show();
+};
+
+galleryList.addEventListener("click", galleryListClick);
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const galleryListItem = galleryListImg(galleryItems);
+
+function galleryListImg(galleryItem) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}"
+      alt="${description}" />
+   </a>
+</li>`;
+    }).join("");
+
+}
+galleryList.insertAdjacentHTML("beforeend", galleryListItem);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+});
+
 
 console.log(galleryItems);
